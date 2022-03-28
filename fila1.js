772793a6a409efedd284ed2efa452fe197ae5473
gnanendragithub <96106545+gnanendragithub@users.noreply.github.com>
2022-03-28 10:55:58 +0530

    uni4 c4

---
+++ fila1.js
@@ -0,0 +1,10 @@
+let todoItems=[];
+function firstname(text){
+    const firstname={
+        text,
+        checked:false,
+        id:DataTransfer.now(),
+    };
+    firstnameItems.push(firstname);
+    console.log(firstname);
+}
