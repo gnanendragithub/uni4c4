a1bdc4785e4642c79d3cb6d52f7c8185b5e472bd
gnanendragithub <96106545+gnanendragithub@users.noreply.github.com>
2022-03-28 11:02:20 +0530

    Update fila1.js

--- fila1.js
+++ fila1.js
@@ -1,4 +1,4 @@
-let todoItems=[];
+let firstname=[];
 function firstname(text){
     const firstname={
         text,
@@ -8,3 +8,56 @@ function firstname(text){
     firstnameItems.push(firstname);
     console.log(firstname);
 }
+const formm=document.querySelector('.js-form');
+formm.addEventListener('submit',event=>{
+    event.preventDefault();
+    const input=document.querySelector('.js-firstname');
+    const text=input.value.trim();
+    if(text!==''){
+        firstname(text);
+        input.value='';
+        input.focus();
+    }
+});
+let lastname=[];
+function lastname(text){
+    const lastname ={
+        text,
+        checked:false,
+        id:DataTransfer.now(),
+    };
+    firstnameItems.push(lastname);
+    console.log(lastname);
+}
+const formm=document.querySelector('.js-form');
+formm.addEventListener('submit',event=>{
+    event.preventDefault();
+    const input=document.querySelector('.js-lastname');
+    const text=input.value.trim();
+    if(text!==''){
+        lastname(text);
+        input.value='';
+        input.focus();
+    }
+});
+let email=[];
+function email(text){
+    const email={
+        text,
+        checked:false,
+        id:DataTransfer.now(),
+    };
+    firstnameItems.push(email);
+    console.log(email);
+}
+const formm=document.querySelector('.js-form');
+formm.addEventListener('submit',event=>{
+    event.preventDefault();
+    const input=document.querySelector('.js-email');
+    const text=input.value.trim();
+    if(text!==''){
+        email(text);
+        input.value='';
+        input.focus();
+    }
+});
